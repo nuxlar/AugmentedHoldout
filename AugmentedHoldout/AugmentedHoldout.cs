380b5286e3b7362b7521f21306330f7d35258c25
Romy Maghsoudi <romy.mag9@gmail.com>
2022-11-08 16:28:37 -0500

    1.0.1

--- AugmentedHoldout/AugmentedHoldout.cs
+++ AugmentedHoldout/AugmentedHoldout.cs
@@ -2,42 +2,55 @@ using BepInEx;
 using BepInEx.Configuration;
 using RoR2;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace AugmentedHoldout
 {
-  [BepInPlugin("com.Nuxlar.AugmentedHoldout", "AugmentedHoldout", "1.0.0")]
+  [BepInPlugin("com.Nuxlar.AugmentedHoldout", "AugmentedHoldout", "1.0.1")]
 
   public class AugmentedHoldout : BaseUnityPlugin
   {
     public ConfigEntry<float> creditMultiplier;
+    public ConfigEntry<bool> enableDuringEclipse;
     public static ConfigFile RoRConfig { get; set; }
     private bool tpStarted = false;
+    private bool shipStarted = false;
     // Monster credit manipulators { Min, Max }
     private readonly float[] monsterCreditBase = { 15, 40 };
     private readonly float[] monsterCreditInterval = { 10, 20 };
     private readonly float[] rerollSpawnInterval = { 2.25f, 4.5f };
     // Timers
     private float monsterCreditTimer = 0;
+    // SpawnCards
+    DirectorCardCategorySelection dccsVoidStageMonsters = Addressables.LoadAssetAsync<DirectorCardCategorySelection>("RoR2/DLC1/voidstage/dccsVoidStageMonsters.asset").WaitForCompletion();
 
     public void Awake()
     {
       RoRConfig = new ConfigFile(Paths.ConfigPath + "\\com.Nuxlar.AugmentedHoldout.cfg", true);
       creditMultiplier = RoRConfig.Bind("General", "Credit Multiplier", 1.25f, "A multiplier for the rate at which more difficult monsters spawn; 1 is the default rate.");
+      enableDuringEclipse = RoRConfig.Bind("General", "TeleExpand Eclipse", false, "Should the teleporter expand after defeating the boss in Eclipse?");
       On.RoR2.HoldoutZoneController.Start += HoldoutZoneControllerStart;
       On.RoR2.HoldoutZoneController.OnDisable += HoldoutZoneControllerOnDisable;
       On.RoR2.CombatDirector.FixedUpdate += CombatDirectorFixedUpdate;
       On.RoR2.CombatDirector.Simulate += CombatDirectorSimulate;
+      On.RoR2.TeleporterInteraction.UpdateMonstersClear += TeleporterInteractionUpdateMonstersClear;
     }
 
     private void HoldoutZoneControllerStart(On.RoR2.HoldoutZoneController.orig_Start orig, RoR2.HoldoutZoneController self)
     {
       orig(self);
       tpStarted = true;
+      if (self.inBoundsObjectiveToken == "OBJECTIVE_MOON_CHARGE_DROPSHIP")
+        shipStarted = true;
+      //"Teleporter1(Clone)" "LunarTeleporter Variant(Clone)"
+      // moon pillar MoonBatteryDesign MoonBatteryBlood MoonBatterySoul MoonBatteryMass (some number)
     }
 
     private void HoldoutZoneControllerOnDisable(On.RoR2.HoldoutZoneController.orig_OnDisable orig, RoR2.HoldoutZoneController self)
     {
       tpStarted = false;
+      if (self.inBoundsObjectiveToken == "OBJECTIVE_MOON_CHARGE_DROPSHIP")
+        shipStarted = false;
       orig(self);
     }
 
@@ -61,6 +74,10 @@ namespace AugmentedHoldout
     {
       if (tpStarted)
       {
+
+        if (shipStarted)
+          self.monsterCards = dccsVoidStageMonsters;
+
         // Update interval
         monsterCreditTimer -= deltaTime;
 
@@ -83,6 +100,22 @@ namespace AugmentedHoldout
         }
       }
       orig(self, deltaTime);
+    }
+    private void TeleporterInteractionUpdateMonstersClear(On.RoR2.TeleporterInteraction.orig_UpdateMonstersClear orig, RoR2.TeleporterInteraction self)
+    {
+      orig(self);
+      //Minimum charge of 5% to prevent it from instantly expanding when the tele starts before boss is spawned
+      if (self.monstersCleared && self.holdoutZoneController && self.activationState == TeleporterInteraction.ActivationState.Charging && self.chargeFraction > 0.05f)
+      {
+        bool eclipseEnabled = Run.instance.selectedDifficulty >= DifficultyIndex.Eclipse2;
+        if (enableDuringEclipse.Value || !eclipseEnabled)
+        {
+          if (Util.GetItemCountForTeam(self.holdoutZoneController.chargingTeam, RoR2Content.Items.FocusConvergence.itemIndex, true, true) <= 0)
+          {
+            self.holdoutZoneController.currentRadius = 1000000f;
+          }
+        }
+      }
     }
   }
 }
--- README.md
+++ README.md
@@ -1,14 +1,38 @@
 # Augmented Holdout
 
-This mod increases spawn rate and available credits during all holdout events. Credit Multiplier is configurable. Planning to replace some zones with a better mechanic.
+This mod increases spawn rate and available credits during all holdout events while also reworking/adding to others. Credit Multiplier and Eclipse TeleExpansion are configurable. Planning to rework Void Locus and Commencement to be more fun areas to explore rather than charging stuff.
+
+All Holdouts:
+  - Increased monster spawn rate
+  - Increased credits for spawning (125% default)
+
+Teleporter Holdouts:
+  - Expands to the whole stage when the boss is defeated and charged to 5%
+  - Has a config option to enable for Eclipse 2+ (off by default)
+
+Moon Escape Holdout:
+  - Removes Lunar Chimera from spawn pool
+  - Adds all void enemies to spawn pool (no voidling)
 
 ## Future Plans
 - Replace holdout zones with actually fun mechanics specific to each holdout zone type
+  - Lunar/Void Pillars
+    - Divide into different areas with incentive to explore and clear rather than making it mandatory
+    - Twip/Gura are the more reasonable scavs spawn either at blood arena
+    - Maybe lunar pillars have set options similar to a chance based buff/debuff event system
 
 ## Credits
-**Sacrifice Remix** Used the spawn rate multiplier from [SacrificeRemix](https://thunderstore.io/package/drizzlx/SacrificeRemix/)
+
+**drizzlx** Used the spawn rate multiplier from [SacrificeRemix](https://thunderstore.io/package/drizzlx/SacrificeRemix/)
+
+**Moffein** Used the TeleExpansion on Boss Defeat from [RiskyMod](https://thunderstore.io/package/Risky_Lives/RiskyMod/)
 
 ## Changelog
+
+**1.0.1**
+
+- Replaces Lunar Chimera on Escape with all Void enemies
+- Adds TeleExpansion on boss defeat and Eclipse config
 
 **1.0.0**
 
